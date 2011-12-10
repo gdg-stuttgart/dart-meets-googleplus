c56150ca708e12d6f91d6be6968ba88499f512a1
dosc <github@strongline.org>
2011-12-10 14:28:23 +0100

    Init2

--- Dart2Google/Dart2Google.dart
+++ Dart2Google/Dart2Google.dart
@@ -1,18 +1,17 @@
 #import('dart:html');
-#source('GooglePlus.dart');
+#import('GooglePlus.dart');
 
 class Dart2Google {
 
   Dart2Google() {
   }
 }
-
-
+  
+final String _MISCHAS_ACCOUNT_ID = "104512463398531242371"; 
 
 void main() {
-  
+   
   GooglePlus gp = new GooglePlus();
-  
-  
-  
+  Person person = gp.findPerson(_MISCHAS_ACCOUNT_ID);
+     
 }
