624aa9cf2836cee69e8b1dedc561d319ca73313f
dosc <github@strongline.org>
2011-12-10 18:05:00 +0100

    Noch ein Commit

--- GooglePlusApi.dart
+++ GooglePlusApi.dart
@@ -1,27 +1,42 @@
 class GooglePlusApi {
 
-  final String _API_KEY = "AIzaSyC8xz1qg0p1XzghRBX1ArNAqpcb1UWuzyQ";
+  final String _API_KEY = "AIzaSyArKhnYYVWdPcEJNci0GuR9Pc_ptkNGA44";
   final String _URL = "https://www.googleapis.com/plus/v1/people/{%1}?pp=1&key={%2}";
   
   GooglePlus() {
   }
 
   Person findPerson(String accountId) {
+    
     String url = _getUrlFor(accountId);
-    doRequest(url);
+    String request = doRequest(url);
+    
+    return new Person(request, "");
   }
   
   String _getUrlFor(String accountId){
     return _URL.replaceFirst("{%1}", accountId).replaceFirst("{%2}", _API_KEY);
   }
 
-  void doRequest(String url){
-  
+  String doRequest(String url){ 
+    
+
     XMLHttpRequest request = new XMLHttpRequest();
-    request.open('GET', url, false); 
+    
+    request.on.readyStateChange.add(void _(Event event) {
+      
+      if (request.readyState == 4) {
+        if (request.status == 200) {
+          print(JSON.parse(request.responseText));
+        } else {
+          print("No");
+        }
+      }
+    });
+
+    request.open('GET', url, true);   
     request.send();
     
-    print(request.status);
-    print(request.responseText);
+    return "";  
   }
 }
